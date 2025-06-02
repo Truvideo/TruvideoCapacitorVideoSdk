835a0e0f9418b880ca0060a48d0d283c3756a9cd
himanshu-rathore-5exe <himanshu.rathore@5exceptions.com>
2025-06-03 01:18:25 +0530

    fix

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,12 +1,55 @@
 export interface TruvideoSdkVideoPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
-  concatVideos(options: { value: string }): Promise<{ value: string }>;
-  encodeVideo(options: { value: string }): Promise<{ value: string }>;
-  getVideoInfo(options: { value: string }): Promise<{ value: string }>;
-  compareVideos(options: { value: string }): Promise<{ value: string }>;
-  mergeVideos(options: { value: string }): Promise<{ value: string }>;
-  cleanNoise(options: { value: string }): Promise<{ value: string }>;
-  editVideo(options: { value: string }): Promise<{ value: string }>;
-  generateThumbnail(options: { value: string }): Promise<{ value: string }>;
-}
+    echo(options: {
+        value: string
+    }): Promise<{ value: string }>;
 
+    concatVideos(options: {
+        videoUris: string;
+        resultPath: string
+    }): Promise<{ result: object }>;
+
+    encodeVideo(options: {
+        videoUri: string;
+        resultPath: string;
+        config: string;
+    }): Promise<{ result: object }>;
+
+    getVideoInfo(options: {
+        videoPath: string;
+    }): Promise<{ result: object }>;
+
+    compareVideos(options: {
+        videoUris: string;
+    }): Promise<{ result: object }>;
+
+    mergeVideos(options: {
+        videoUris: string;
+        resultPath: string;
+        config: string;
+    }): Promise<{ result: object }>;
+
+    cleanNoise(options: {
+        videoPath: string;
+        resultPath: string;
+    }): Promise<{ result: object }>;
+
+    editVideo(options: {
+        videoPath: string;
+        resultPath: string;
+        config: string;
+    }): Promise<{ result: object }>;
+
+    generateThumbnail(options: {
+        videoPath: string;
+        resultPath: string;
+        config: string;
+        position: number;
+        width: number;
+        height: number;
+        precise: boolean;
+    }): Promise<{ result: object }>;
+
+    getResultPath(options: {
+        path: string
+    }): Promise<{ resultPath: string }>;
+}
