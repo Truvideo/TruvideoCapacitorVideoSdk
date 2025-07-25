9cff79cc88fd24291832351f8c5807c1b86d6909
Himanshu <premanshu.solanki@cellulant.io>
2025-07-25 22:05:18 +0530

    fix

--- dist/plugin.cjs.js
+++ dist/plugin.cjs.js
@@ -94,22 +94,22 @@ class MergeBuilder {
             config: JSON.stringify(config),
         });
         console.log("📥 [Build] mergeVideos API Raw Response:", response);
-        // if (!response?.result || typeof response.result !== 'object') {
-        //     throw new Error('❌ mergeVideos result is not a valid object.');
-        // }
         // this.mergeData = response.result as BuilderResponse;
         try {
+            if (!(response === null || response === void 0 ? void 0 : response.result) || typeof response.result !== 'object') {
+                throw new Error('❌ mergeVideos result is not a valid object.');
+            }
             this.mergeData = response.result;
+            if (!this.mergeData.id) {
+                throw new Error('❌ mergeVideos result is missing `id`.');
+            }
+            console.log("✅ [Build] MergeBuilder build success. MergeData:", this.mergeData);
+            console.log("🔁 [Build] Returning instance of MergeBuilder:", this);
         }
         catch (e) {
             console.error("❌ [Process] Failed to parse resultPath JSON:", response.result, e);
             throw new Error('❌ Failed to parse resultPath from MergeBuilder.');
         }
-        if (!this.mergeData.id) {
-            throw new Error('❌ mergeVideos result is missing `id`.');
-        }
-        console.log("✅ [Build] MergeBuilder build success. MergeData:", this.mergeData);
-        console.log("🔁 [Build] Returning instance of MergeBuilder:", this);
         return this;
     }
     async process() {
